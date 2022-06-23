bba77b2de026c06163ac7ce9fadae7600660206a
mirajul alam <alifmirajulalam@gmail.comm>
2022-06-23 23:15:21 +0600

    portfolio section added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
 import './portfolio.css';
+import IMG1 from '../../assets/live.jpg'
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+
+          <article className='portfolio_item'>
+            <div className="portfolio_item-image">
+               <img src={IMG1} alt="" />
+            </div>
+              <h3>This is portfolio item title</h3>
+             <div className="portfolio_item-cta">
+             <a href="https://github.com/mirajulalam" className='btn' target="_blank">Live</a>
+             <a href="https://github.com/mirajulalam" className='btn' target="_blank">Server</a>
+              <a href="https://car-parts-manufacturer.web.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+             </div>
+          </article>
+          <article className='portfolio_item'>
+            <div className="portfolio_item-image">
+               <img src={IMG1} alt="" />
+            </div>
+              <h3>This is portfolio item title</h3>
+             <div className="portfolio_item-cta">
+             <a href="https://github.com/mirajulalam" className='btn' target="_blank">Live</a>
+             <a href="https://github.com/mirajulalam" className='btn' target="_blank">Server</a>
+              <a href="https://car-parts-manufacturer.web.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+             </div>
+          </article>
+          <article className='portfolio_item'>
+            <div className="portfolio_item-image">
+               <img src={IMG1} alt="" />
+            </div>
+              <h3>This is portfolio item title</h3>
+             <div className="portfolio_item-cta">
+             <a href="https://github.com/mirajulalam" className='btn' target="_blank">Live</a>
+             <a href="https://github.com/mirajulalam" className='btn' target="_blank">Server</a>
+              <a href="https://car-parts-manufacturer.web.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+             </div>
+          </article>
+
+          
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import './testimonials.css';
+import AVATAR1 from '../../assets/IMG-20220503-WA0079.jpg';
+import AVATAR2 from '../../assets/review2.jfif';
+import AVATAR3 from '../../assets/review3.jfif';
+
+
+
 const Testimonials = () => {
+
+  const data =[
+    {
+        name: 'Mary',
+        review: "I had a little hickup with processing my order. And customer service was stellar. We had an open line of communication and they resolved the issue in a timely manor. Answered all of my questions and concerns. I personally believe one of the best features a company should strive for is relationships with costomers and willingness to work together to resolve issues.",
+        avatar: AVATAR1
+    },
+    {
+        name: 'Harry',
+        review: "After I submitted a low rating due to a shipping delay & faulty part Rose gave me a call within a few hours of submitting my rating & worked diligently to get things straightened out. Originally I had written I wouldn't buy from them again, but seeing how great their customer service is its reassuring to know that if you have an issue they will ensure to fix your issue",
+        avatar: AVATAR1
+    },
+    {
+        name: 'Jennifer',
+        review: "I made contact with customer service an they were extremely helpful to get my issue resolved. I explain to them that it was very dented probably from shipping an they are currently shipping me another one free to me. I am very surprised how this company handles their customers an are not all about a dollar. Definitely will be buying more parts. ",
+        avatar: AVATAR1
+    },
+]
+
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials_container">
+        
+       {
+        data.map(({avatar,name,review},index)=>{
+          return(
+            <article key={index} className='testimonials'>
+          <div className="clients_avatar">
+            <img src={avatar} alt="avatar1" />
+          </div>
+          <h5 className='client_name'>{name}</h5>
+            <small className='client_review'>{review}</small>
+        </article>
+          )
+        })
+       }
+        
+      </div>
+    </section>
   )
 }
 
